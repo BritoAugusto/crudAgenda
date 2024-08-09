b2ef6f143328dd47ff9cdf596151a12f3b2dc822
Augusto Brito <britoaugusto1@gmail.com>
2024-08-09 20:19:06 -0300

    puedo usar la clase contacto en admin js

---
+++ js/admin.js
@@ -0,0 +1,15 @@
+import { Contacto } from "./classContacto.js";
+
+console.log("Hola mundo");
+
+const crearContacto = new Contacto(
+  1,
+  "Augusto",
+  "Brito",
+  "Chino",
+  "las talitas",
+  "38155564",
+  "chino@mail.com",
+  "link"
+);
+console.log(crearContacto);
