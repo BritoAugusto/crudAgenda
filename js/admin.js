105ba8ec3622581671ff6e23e83a1b32eb4316bb
Augusto Brito <britoaugusto1@gmail.com>
2024-08-09 21:59:16 -0300

    creo un contacto que se agrega a la listaContacto y limpia el formulario

--- js/admin.js
+++ js/admin.js
@@ -1,24 +1,45 @@
 import { Contacto } from "./classContacto.js";
 
 //crear variables
-const crearContacto = new Contacto(
-  undefined,
-  "Augusto",
-  "Brito",
-  "Chino",
-  "las talitas",
-  "38155564",
-  "chino@mail.com",
-  "link"
-);
+
 const modalAdminContacto = new bootstrap.Modal (document.querySelector("#modalContacto"));
 const btnAgregarContacto = document.querySelector('.btn-primary');
-
+const formRegistrarContacto = document.querySelector('form');
+const nombre = document.querySelector('#nombre'); 
+const apellido = document.querySelector('#apellido'); 
+const apodo = document.querySelector('#apodo'); 
+const email = document.querySelector('#email'); 
+const celular = document.querySelector('#celular'); 
+const foto = document.querySelector('#foto'); 
+const direccion = document.querySelector('#direccion'); 
+const listaContactos = [];
 //funciones
 const mostrarModal = ()=>{
     modalAdminContacto.show()
 }
 
+const crearContacto = (e)=>{
+    e.preventDefault();
+    const contactoNuevo = new Contacto(
+      undefined,
+      nombre.value,
+      apellido.value,
+      apodo.value,
+      direccion.value,
+      celular.value,
+      email.value,
+      foto.value
+    );
+    console.log(contactoNuevo);
+    listaContactos.push(contactoNuevo)
+    console.log(listaContactos)
+    limpiarFormulario();
+}
+
+const limpiarFormulario = ()=>{
+    formRegistrarContacto.reset();
+}
 
 //agregar los manejadores de eventos
-btnAgregarContacto.addEventListener('click', mostrarModal)
+btnAgregarContacto.addEventListener('click', mostrarModal);
+formRegistrarContacto.addEventListener('submit', crearContacto);
